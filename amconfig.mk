6051a862a86387c508f99c4a08c693c7ccae7e5a
secuflag <secuflag@ticino.com>
2017-12-19 15:13:31 +0100

    First import
     * apns-conf.xml picked from https://github.com/LineageOS/android_vendor_lineage/blob/staging/lineage-15.1/prebuilt/common/etc/apns-conf.xml
     * hosts file picked from http://winhelp2002.mvps.org/hosts.txt

---
+++ amconfig.mk
@@ -0,0 +1,50 @@
+#
+# AOSPMod versioning
+#
+
+AM_VERSION := OREO-R.01
+AOSP_TAG_ID := android-8.1.0_r2
+
+# Common overlay
+#PRODUCT_PACKAGE_OVERLAYS += \
+#    vendor/aospmod/overlay/
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.com.android.dateformat=dd-MM-yyyy
+
+# SetupWizard
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.enterprise_mode=1
+
+# Enable assistant on all devices
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true
+
+# Expose SELinux settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/aospmod/extra/apns-conf.xml:system/etc/apns-conf.xml
+
+# ADBlock file
+PRODUCT_COPY_FILES += \
+    vendor/aospmod/extra/hosts:system/etc/hosts
+
+# LineageOS packages
+#PRODUCT_PACKAGES += \
+#    Eleven
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.aospmod.version=$(AM_VERSION) \
+    ro.aosptag.id=$(AOSP_TAG_ID)
+
+# Include Google Apps
+#include vendor/google/gapps/gapps.mk
